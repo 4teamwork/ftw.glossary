0a22c4139ae28479bf0e38f8cd6a55b462e4f772
Lukas Graf <code@lukas-graf.ch>
2010-09-12 22:38:15 +0200

    Implemented search for glossary terms (without AJAX)

--- ftw.glossary/ftw/glossary/browser/glossaryview.py
+++ ftw.glossary/ftw/glossary/browser/glossaryview.py
@@ -12,12 +12,22 @@ from ftw.glossary import glossaryMessageFactory as _
 class IGlossaryView(Interface):
     """
     Glossary view interface
+
     """
 
-    def search(self, pattern):
-        """Return those terms from the catalog that match pattern
+    def matching_terms(self, term):
+        """
+        Return those terms from the catalog that match pattern
 
         """
+    def search_results(self):
+        """
+        Search for GlossaryItems matching `self.search_term` and return a
+        list of dicts with terms and definitions
+
+        """
+
+
 
 
 class GlossaryView(BrowserView):
@@ -30,29 +40,44 @@ class GlossaryView(BrowserView):
     template = ViewPageTemplateFile('glossaryview.pt')
 
     def __call__(self):
-        # TODO: This is an attempt at a self-submitting form, but this method
-        #       doesn't seem to get called at all yet.
+        """
+        Self-submitting form that displays a search field and
+        results from the search
+        """
         form = self.request.form
+        self.search_term = ''
+
         # Make sure we had a proper form submit, not just a GET request
         submitted = form.get('form.submitted', False)
-
         search_button = form.get('glossary-search-button', None) is not None
         if submitted and search_button:
-            # Return search results
-            # self.request.response.redirect(self.context.absolute_url())
-            return ''
-        else:
-            return self.template()
+            # Set search_term, to be used by search_results()
+            self.search_term = form.get('glossary-search-field', '')
+        return self.template()
 
-    def search(self, term=None):
-        """
-        Search for GlossaryItems matching `pattern` and return a JSONified list 
-
-        """
+    def matching_terms(self, term=None):
+        """Search for GlossaryItems matching `term` and return a JSON list"""
         if term is None:
             return []
         else:
             catalog = getToolByName(self.context, 'portal_catalog')
-            terms = [brain.Title for brain in catalog(portal_type='GlossaryItem',
-                                                      Title = "%s*" % term)]
+            terms = [brain.Title for brain in catalog(
+                portal_type='GlossaryItem',
+                Title = "%s*" % term)]
             return json.dumps(terms)
+
+    def search_results(self):
+        """
+        Search for GlossaryItems matching `self.search_term` and return a
+        list of dicts with terms and definitions
+
+        """
+        results = []
+        if self.search_term != '':
+            catalog = getToolByName(self.context, 'portal_catalog')
+            for brain in catalog(portal_type='GlossaryItem',
+                                 Title = "%s*" % self.search_term):
+                results.append(dict(term=brain.Title,
+                                    description=brain.description))
+        return results
+
