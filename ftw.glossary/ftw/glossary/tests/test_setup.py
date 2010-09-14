2646e40c8f3bfa23e5e71c5235382582da6eeb3a
Lukas Graf <lukas.graf@4teamwork.ch>
2010-09-14 17:45:12 +0200

    Added unit test for GlossaryItem creation

--- ftw.glossary/ftw/glossary/tests/test_setup.py
+++ ftw.glossary/ftw/glossary/tests/test_setup.py
@@ -9,6 +9,11 @@ class TestSetup(FtwGlossaryTestCase):
         self.workflow = getToolByName(self.portal, 'portal_workflow')
         self.acl_users = getToolByName(self.portal, 'acl_users')
         self.types = getToolByName(self.portal, 'portal_types')
+        
+        self.setRoles(('Manager',))
+        self.portal.invokeFactory("Folder", "glossary")
+        self.setRoles(('Member',))
+        
 
     def test_add_glossary_item_permissison(self):
         # The API of the permissionsOfRole() function sucks - it is bound too
@@ -25,7 +30,15 @@ class TestSetup(FtwGlossaryTestCase):
     def test_glossary_item_fti(self):
         document_fti = getattr(self.types, 'GlossaryItem')
         self.failUnless(document_fti.global_allow)
-
+        
+    def test_glossary_item_creation(self):
+        self.setRoles(('Manager',))
+        self.portal.glossary.invokeFactory("GlossaryItem", "html")
+        item_html = self.portal.glossary.html
+        item_html.term = "HTML"
+        item_html.description = "Hyper Text Markup Language"
+        self.assertEquals(item_html.term, "HTML")
+        self.assertEquals(item_html.description, "Hyper Text Markup Language")
 
 
 def test_suite():
